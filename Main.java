77c041f9f79da96ba20560ced917103522f56efd
DanTov72 <105073241+DanTov72@users.noreply.github.com>
2022-05-08 23:35:18 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int num = rand.nextInt(100);
+        System.out.println(num);
+    }
+}
